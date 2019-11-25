cfdb0b655cbaaca0fb15c5e690e8a726f0e88a3a
Gianni <gianni.brouwers@student.kdg.be>
2019-11-25 15:16:44 +0100

    Added Jquery exercises

---
+++ les-4-jquery/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Les 4 Jquery</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script  src="script.js"></script>
+
+    <script>
+        $(document).ready(function(){
+
+            var clrArray = ["blue", "green", "red", "magenta", "yellow", "cyan", "lime", "orange", "purple"];
+
+            // add css to button:
+            // Gets the number of elements with class yourClass
+            var numItems = $('.colorBlocks').length
+
+            for (var i = 0; i <= numItems+1; i++) {
+                var randomValue = clrArray[Math.floor(Math.random()*clrArray.length)];
+
+                if(i === 0){}
+                else{
+                    $("#block" + i).css("background-color", randomValue)
+                    $("#block" + i).append("Your color is " + randomValue);
+                }
+            }
+
+            // when submitButton is clicked:
+            $("#submitColors").click(function(){
+                $("#block1").fadeIn();
+                $("#submitColors").hide();
+                $("#block2").fadeIn("slow");
+                $("#block3").fadeIn(3000);
+                $("#retryButton").fadeIn(4000);
+
+                console.log("test123");
+
+            });
+
+            // if retrybutton is clicked:
+            $("#retryButton").click(function(){
+                location.reload();
+            });
+
+        });
+
+    </script>
+
+
+</head>
+<body>
+
+<p>
+
+<div class="heading">
+    <h3 class="colortitle"> Test your luck try to get 3 of the same colors </h3>
+    <button type="submit" id="submitColors">Submit colors</button>
+    <button type="submit" id="retryButton">Try again!</button>
+</div>
+
+</p>
+
+
+<div id="colorList">
+
+    <div id="block1" class="colorBlocks"><p></p></div>
+    <div  id="block2" class="colorBlocks"><p></p></div>
+    <div  id="block3" class="colorBlocks"><p></p></div>
+</div>
+
+
+
+
+<div class="object"></div>
+
+
+
+</body>
+</html>
